6ea20bae36600a1c511b6cc6f6dc164e92448ff6
priyapawarrao <priya.pawar@sjsu.edu>
2016-04-20 14:39:24 -0700

    Agent REST endpoints updated

--- CloudEnabledDevelopment/src/main/resources/agentScripts/executejar.sh
+++ CloudEnabledDevelopment/src/main/resources/agentScripts/executejar.sh
@@ -1,4 +1,4 @@
-sudo docker exec $1  /bin/sh -c "nohup java -jar /agentScripts/gs-rest-service-0.1.0.jar &"
+sudo docker exec $1  /bin/sh -c "nohup java -jar /agentScripts/gs-rest-service-0.1.0.jar & > agent.log"
 #sudo docker exec -it $1 bash
 
 #echo "Executing the jar"
