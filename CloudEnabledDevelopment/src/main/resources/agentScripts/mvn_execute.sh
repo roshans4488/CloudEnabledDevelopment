17f1266be590b54ab0e1322edde34cad11984e06
Roshan <roshans.4488@gmail.com>
2016-04-24 18:29:12 -0700

    mvn_execute and cloud dev agent jar changes

--- CloudEnabledDevelopment/src/main/resources/agentScripts/mvn_execute.sh
+++ CloudEnabledDevelopment/src/main/resources/agentScripts/mvn_execute.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd $1
-mvn clean package
-java -jar target/*.jar > /agent/logs/$2_execute.log
+mvn clean package -Dmaven.test.skip=true > /agent/logs/$2_package.log
+java -jar target/*.jar >  /agent/logs/$2_execute.log
